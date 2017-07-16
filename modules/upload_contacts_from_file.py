0a5fba9406dff6b856590670ab77a5e079a0d447
Marcus A. Davis <marcus.a.davis24@gmail.com>
2017-07-16 14:38:32 -0700

    Add all Contact fields except dates, datetimes, and Groups to new Contact creation

--- modules/upload_contacts_from_file.py
+++ modules/upload_contacts_from_file.py
@@ -1,4 +1,5 @@
 import csv
+import re
 from management.models import Contact
 
 def csv_upload(filepath):
@@ -18,7 +19,7 @@ def make_contact_dict(row):
 	new_dict["name"] = row["Name"]
 	new_dict["phone_number"] = row["Phone Number"]
 	new_dict["alt_phone_number"] = row["Alternative Phone"]
-	new_dict["delay_in_days"] = row["Delay in days"]
+	new_dict["delay_in_days"] = delay_num_fix(row["Delay in days"])
 	new_dict["language_preference"] = row["Language Preference"]
 	
 	# Dates to be handled to ensure right date format is entered, YYYY-MM-DD
@@ -26,9 +27,73 @@ def make_contact_dict(row):
 	# new_dict["date_of_sign_up"] = row["Date of Sign Up"]
 	# new_dict["functional_date_of_birth"] = row["Functional DoB"]
 
+	# Personal Info
+	new_dict["gender"] = row["Gender"]
+	new_dict["mother_tongue"] = row["Mother Tongue"]
+	new_dict["language_preference"] = row["Language Preference"]
+	new_dict["religion"] = row["Religion"]
+	new_dict["state"] = row["State"]
+	new_dict["division"] = row["Division"]
+	new_dict["district"] = row["District"]
+	new_dict["city"] = row["City"]
+	new_dict["monthly_income_rupees"] = monthly_income(row["Monthly Income"])
+	new_dict["children_previously_vaccinated"] = previous_vaccination(row["Previously had children vaccinated"])
+	new_dict["not_vaccinated_why"] = row["If not vaccinated why"]
+	new_dict["mother_first_name"] = row["Mother's First"]
+	new_dict["mother_last_name"] = row["Mother's Last"]
 
-	# Fix delay in days to fill blanks
-	if not new_dict["delay_in_days"]:
-		new_dict["delay_in_days"] = 0
-		
+
+	# Type of Sign Up
+	new_dict["method_of_sign_up"] = row["Method of Sign Up"]
+	new_dict["org_sign_up"] = row["Org Sign Up"]
+	new_dict["hospital_name"] = row["Hospital Name"]
+	new_dict["doctor_name"] = row["Doctor Name"]
+	new_dict["url_information"] = row["URL information"]
+
+	# System Identification
+	new_dict["telerivet_contact_id"] = row["Telerivet Contact ID"]
+	new_dict["trial_id"] = row["Trial ID"]
+	new_dict["trial_group"] = row["Trial Group"]
+
+	# Message References
+	new_dict["preferred_time"] = row["Preferred Time"]
+	new_dict["script_selection"] = row["Script Selection"]
+	new_dict["telerivet_sender_phone"] = row["Sender Phone"]
+
+	# DateTimes to be fixed
+	# new_dict["last_heard_from"] = row["Last Heard From"]
+	# new_dict["last_contacted"] = row["Last Contacted"]
+	# new_dict["telerivet_time_created"] = row["Time Created"]
+
 	return new_dict
+
+
+def previous_vaccination(row_entry):
+	if "Y".lower() in row_entry:
+		bool_val = True
+	elif "N".lower() in row_entry:
+		bool_val = False
+	else:
+		bool_val = None
+
+	return bool_val
+
+def monthly_income(row_entry):
+	if not row_entry:
+		inc_val = 999999
+	elif re.search("\D+", row_entry):
+		inc_val = 999999
+	else:
+		inc_val = int(row_entry)
+
+	return inc_val
+
+def delay_num_fix(row_entry):
+	if not row_entry:
+		delay = 0
+	elif re.search("\D+", row_entry):
+		delay = 0
+	else:
+		delay = int(row_entry)
+
+	return delay
